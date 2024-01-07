80de73cc579c88ee0669aa881a604bd84622454f
Nuriya Akhmedshina <nyriya36@gmail.com>
2024-01-07 08:27:52 +0500

    Lesson11

---
+++ app/src/main/java/com/selva/composition/presentation/GameViewModelFactory.kt
@@ -0,0 +1,6 @@
+package com.selva.composition.presentation
+
+class GameViewModelFactory(
+
+) {
+}
--- app/src/main/java/com/selva/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/selva/composition/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.selva.composition.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.selva.composition.R
+import com.selva.composition.databinding.FragmentWelcomeBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
